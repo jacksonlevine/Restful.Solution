d08b95ceb83f86357d0a08d0af3d509386a99766
jacksonlevine <jack@netbug.us>
2023-03-01 09:52:28 -0800

    pass date test

--- Restful/Models/Entry.cs
+++ Restful/Models/Entry.cs
@@ -1,8 +1,14 @@
+using System;
 namespace Restful.Models
 {
   public class Entry
   {
     public string Message { get; set; }
     public string Name { get; set; }
+    public string Date { get; set; }
+    public Entry()
+    {
+      Date = DateTime.Now.ToLongDateString();
+    }
   }
 }
